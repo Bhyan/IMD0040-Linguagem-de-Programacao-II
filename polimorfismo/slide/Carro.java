20c2e730e3f6f19e07d0c8f34bda673e11e857d5
Bryan Brito <bhyanbrito+github@gmail.com>
2018-09-05 15:43:39 -0300

    Documentação.

--- polimorfismo/slide/Carro.java
+++ polimorfismo/slide/Carro.java
@@ -1,13 +1,30 @@
+/**
+ * Classe Carro.
+ *
+ * @author Bryan Brito
+ * @version 03.09.2018
+ */
 public class Carro extends Veiculo{
     private int m_chassi;
     private boolean m_quatroPortas;
 
+    /**
+     * Construtor da classe Carro.
+     * @param marca_ Marca do carro.
+     * @param modelo_ Modelo do carro.
+     * @param ano_ Ano do carro.
+     * @param chassi_ Número do chassi do carro.
+     * @param quatroPortas_ Se o carro e quatro portas ou não.
+     */
     public Carro(String marca_, String modelo_, int ano_, int chassi_, boolean quatroPortas_){
         super(marca_, modelo_, ano_);
         m_chassi = chassi_;
         m_quatroPortas = quatroPortas_;
     }
 
+    /**
+     * Mostra os dados do carro.
+     */
     public void imprimirDados(){
         super.imprimirDados();
         System.out.println("Chassi: " + m_chassi);
--- polimorfismo/slide/Veiculo.java
+++ polimorfismo/slide/Veiculo.java
@@ -1,18 +1,36 @@
+/**
+ * Classe Veiculo.
+ *
+ * @author Bryan Brito
+ * @version 03.09.2018
+ */
 public class Veiculo{
     private String m_marca;
     private String m_modelo;
     private int m_ano;
 
+    /**
+     * Construtor da classe Veiculo.
+     * @param marca_ Marca do veiculo.
+     * @param modelo_ Modelo do veiculo.
+     * @param ano_ Ano do veiculo.
+     */
     public Veiculo(String marca_, String modelo_, int ano_){
         m_marca = marca_;
         m_modelo = modelo_;
         m_ano = ano_;
     }
 
+    /**
+     * Buzina do veiculo.
+     */
     public void buzinar(){
         System.out.println("Buzina veiculo.");
     }
 
+    /**
+     * Mostra os dados do veiculo.
+     */
     public void imprimirDados(){
         System.out.println("Marca: " + m_marca);
         System.out.println("Modelo: " + m_modelo);
